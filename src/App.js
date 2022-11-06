0c0d783aa9c738044f875dcfb0c81bf878c4bce9
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2022-11-06 17:32:32 +0100

    first of react js commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+    this.state = {
+      todoItems: [
+        {
+          action: "zadanie 1",
+          done: false,
+        },
+        {
+          action: "zadanie 2",
+          done: true,
+        },
+        {
+          action: "zadanie 3",
+          done: true,
+        },
+        {
+          action: "zadanie 4",
+          done: false,
+        },
+      ],
+      newItemText: "",
+    };
+  }
+
+  updateNexTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  };
+
+  createNewTodo = () => {
+    if (
+      !this.state.todoItems.find(
+        (item) => item.action === this.state.newItemText
+      )
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          {
+            action: this.state.newItemText,
+            done: false,
+          },
+        ],
+        newItemText: "",
+      });
+    }
+  };
+
+  toggleTodo = (todo) => {
+    this.setState({
+      todoItems: this.state.todoItems.map((item) =>
+        item.action === todo.action ? { ...item, done: !item.done } : item
+      ),
+    });
+  };
+
+  todoTableRows = () =>
+    this.state.todoItems.map((item) => (
+      <tr key={item.action}>
+        <td>{item.action}</td>
+        <td>
+          <input
+            type="checkbox"
+            checked={item.done}
+            onChange={() => this.toggleTodo(item)}
+          />
+        </td>
+      </tr>
+    ));
+
+  render = () => {
+    return (
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">
+          lista zadań użytkownika {this.state.userName}
+          (Liczba zadań: {this.state.todoItems.filter((a) => !a.done).length})
+        </h4>
+        <div className="container-fluid">
+          <div className="my-1">
+            <input
+              className="form-control"
+              value={this.state.newItemText}
+              onChange={this.updateNexTextValue}
+            />
+            <button
+              className="btn btn-primary mt-1"
+              onClick={this.createNewTodo}
+            >
+              Dodaj
+            </button>
+          </div>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>opis</th>
+                <th>Wykonane</th>
+              </tr>
+            </thead>
+            <tbody>{this.todoTableRows()}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
